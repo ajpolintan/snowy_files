0f6a821ac3129c6be8904068c286dec942177f3d
Allen Jake Polintan <ajpolintan@dons.usfca.edu>
2025-07-28 16:03:06 -0700

    added draggable windows and fixed links

--- app/ui/DraggableWindow.tsx
+++ app/ui/DraggableWindow.tsx
@@ -1,26 +1,21 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useRef,ReactNode } from 'react';
 import Draggable from 'react-draggable'; // The default
 
 interface DraggableWindowProps {
   onClose: () => void; // onClose is a function that takes no args and returns nothing
+  children: ReactNode;
 }
 
-const DraggableWindow: React.FC<DraggableWindowProps> = ({ onClose }) => {
+const DraggableWindow: React.FC<DraggableWindowProps> = ({ onClose, children }) => {
 
     const nodeRef = useRef(null);
 
     return (
         <Draggable nodeRef={nodeRef}> 
-            <div ref={nodeRef} className ="flex border-b border-gray-500 text-2xl mb-3"> 
-                <p> Hi my name is Allen Jake Polintan</p>
-                <ul> My favorite hobbies are
-                    <li> gaming </li>
-                    <li> gaming </li>
-                    <li> gaming </li>
-
-                </ul>
-                        <button onClick={onClose} className="text-red-500">✕</button>
+            <div ref={nodeRef} className ="relative flex flex-col border-5 border-purple-500 text-2xl mb-3 bg-black"> 
+                <button onClick={onClose} className="absolute top-0 right-2 text-red-500">✕</button>
+                { children }
             </div>
         </Draggable> 
     )
---
+++ app/ui/contents/about.tsx
@@ -0,0 +1,23 @@
+export default function About() {
+    return (
+    <div>
+
+    <h1 className=" mb-16 "> 
+                    Hi my name is Allen Jake Polintan
+                </h1>
+                <h1>
+                    Education
+                </h1>
+                <h1>
+                    My interests!
+                </h1>
+                <ul  className="list-disc"> 
+                    <li> Dance Dance Revolution </li>
+                    <li> Soulslikes and JRPGs (Final Fantasy :) </li>
+                    <li> Reading Fantasy and YA Novels</li>
+                    <li> Playing Guitar </li>
+                    <li> JPOP (Yorushika) + KPOP listener (IVE, BTS) </li>
+                </ul>
+        </div>
+    )
+}
